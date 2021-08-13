c9e1d7adb93a9fca871493e1970a0e33df2a46de
GoncharovFyodor <44238095+GoncharovFyodor@users.noreply.github.com>
2021-08-14 01:21:25 +0300

    HW07_1

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,13 +2,19 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.meal1;
+import static ru.javawebinar.topjava.MealTestData.meals;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -29,4 +35,30 @@ class RootControllerTest extends AbstractControllerTest {
                         }
                 ));
     }
+
+    @Test
+    public void getMeals() throws Exception {
+        List<MealTo> mealTos = MealsUtil.getFilteredTos(meals, MealsUtil.DEFAULT_CALORIES_PER_DAY,
+                meal1.getTime(), meal1.getTime());
+
+        MealTo mealTo = !mealTos.isEmpty() ? mealTos.get(0) : null;
+
+        if (mealTo == null) throw new Exception("mealWithExceed == null");
+
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(mealTo.getId())),
+                                hasProperty("dateTime", is(mealTo.getDateTime())),
+                                hasProperty("description", is(mealTo.getDescription())),
+                                hasProperty("calories", is(mealTo.getCalories())),
+                                hasProperty("exceed", is(mealTo.isExcess()))
+                        ))));
+    }
+
 }
